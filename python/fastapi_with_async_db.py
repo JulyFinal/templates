d7e445edaa081967a79d3380f15a6aa8e20d5fb3
Final <14924875+JulyFinal@users.noreply.github.com>
2024-03-25 09:14:24 +0000

    update fastapi template

--- python/fastapi_with_async_db.py
+++ python/fastapi_with_async_db.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from asyncio import current_task
 import datetime
 from typing import Annotated
 
@@ -14,17 +15,15 @@ from sqlalchemy import String, Integer, func, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
+    async_scoped_session,
     AsyncSession,
     AsyncAttrs,
     create_async_engine,
+    AsyncEngine,
 )
 
 
-URL_DATABASE = "mysql+aiomysql://final:final4188@192.168.168.72/test_case"
-engine = create_async_engine(URL_DATABASE)
-SessionLocal = async_sessionmaker(engine)
-
-
+# ============= model ===================
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
@@ -41,22 +40,74 @@ class UserBase(BaseModel):
     name: str
 
 
+# ================ database ===============
+URL_DATABASE = "sqlite+aiosqlite:///test.db"
+
+
+# Import statements...
+class DatabaseSessionManager:
+    def __init__(self, db_url: str):
+        self.URL_DATABASE = db_url
+        self.engine: AsyncEngine | None = None
+        self.session_maker = None
+        self.session = None
+
+    def init_db(self):
+        # Database connection parameters...
+
+        # Creating an asynchronous engine
+        self.engine = create_async_engine(self.URL_DATABASE)
+
+        # Creating an asynchronous session class
+        self.session_maker = async_sessionmaker(
+            autocommit=False, autoflush=False, bind=self.engine
+        )
+        # self.engine = create_async_engine(
+        #     database_url, pool_size=100, max_overflow=0, pool_pre_ping=False
+        # )
+
+        # Creating a scoped session
+        self.session = async_scoped_session(self.session_maker, scopefunc=current_task)
+
+    async def create_tables(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async def close(self):
+        # Closing the database session...
+        if self.engine is None:
+            raise Exception("DatabaseSessionManager is not initialized")
+        await self.engine.dispose()
+
+
+# Initialize the DatabaseSessionManager
+sessionmanager = DatabaseSessionManager(URL_DATABASE)
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    sessionmanager.init_db()
+    sessionmanager.create_tables()
     yield
+    await sessionmanager.close()
 
 
 app = FastAPI(lifespan=lifespan)
 
 
 async def get_db():
-    db = SessionLocal()
+    session = sessionmanager.session()
+    if session is None:
+        raise Exception("DatabaseSessionManager is not initialized")
     try:
-        yield db
+        # Setting the search path and yielding the session...
+        yield session
+    except Exception:
+        await session.rollback()
+        raise
     finally:
-        await db.close()
+        # Closing the session after use...
+        await session.close()
 
 
 db_dependency = Annotated[AsyncSession, Depends(get_db)]
@@ -79,7 +130,7 @@ async def get_users(db: db_dependency):
 
 @app.get("/user")
 async def get_user(id: int, db: db_dependency):
-    user = await db.scalar_one(select(User).where(User.id == id))
+    user = await db.scalar(select(User).where(User.id == id))
     return user
 
 
